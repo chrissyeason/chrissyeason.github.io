392fe877ea35b3c2d8897bcfa54fb36166d4e73c
chrissyeason <chrissyeason@gmail.com>
2019-06-27 23:01:16 -0500

    geocode added

--- brew-finder/app.js
+++ brew-finder/app.js
@@ -1,28 +1,34 @@
 // $('body').append('im working');
 $('#submit-city').click(function(e){
-    // e.preventDefault();
-    // const userInput = $('#city-input').val();
-    // // split string into state and city
-    // // userinput = 'austin, tx'
-    // var inputArray = userInput.split(',');
-    // // inputArray[0] = 'austin'
-    // // inputArray[1] = ' tx'
-    // // loop through array remove any spaces
-    // for(let i = 0; i < inputArray.length; i++){
-    //     inputArray[i] = inputArray[i].replace(' ', '');
-    // }
+    e.preventDefault();
+    const userInput = $('#city-input').val();
+    // split string into state and city
+    // userinput = 'austin, tx'
+    var inputArray = userInput.split(',');
+    // inputArray[0] = 'austin'
+    // inputArray[1] = ' tx'
+    // loop through array remove any spaces
+    for(let i = 0; i < inputArray.length; i++){
+        inputArray[i] = inputArray[i].replace(' ', '');
+    }
     
 
     $.ajax({
-        url:`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&types=zoo&key=AIzaSyDSiSgfYZAiH2o18cKrHdoWSdTumTNUqgw`,
+        url: `http://dev.virtualearth.net/REST/v1/Locations?countryRegion=US&adminDistrict=${inputArray[1]}&locality=${inputArray[0]}&key=AsNSrJ9nxY0hH82Hft6W7lSFK5zKER34LGJ3hYm70N6buBQXYlVneYV22zyYVLjL`,
+        // url:`https://api.brewerydb.com/v2/&key=68855133f4391d6efa6f4d8fcf1b7674`
+        // ,
     }).then(
         (data) => {
-            console.log(data);
+            const searchCoordinates = data.resourceSets[0].resources[0].geocodePoints[0].coordinates;
         },
         () => {
             console.log("bad request");
         }
     );
 })
+
+
+
+
 
     
