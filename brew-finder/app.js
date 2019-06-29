9f8e317d3797f209d32dada0ece452944c3efe0a
chrissyeason <chrissyeason@gmail.com>
2019-06-29 12:01:21 -0500

    parsed string and got brewery info

--- brew-finder/app.js
+++ brew-finder/app.js
@@ -17,13 +17,19 @@ $('#submit-city').click(function(e){
         url: `https://dev.virtualearth.net/REST/v1/Locations?countryRegion=US&adminDistrict=${inputArray[1]}&locality=${inputArray[0]}&key=AsNSrJ9nxY0hH82Hft6W7lSFK5zKER34LGJ3hYm70N6buBQXYlVneYV22zyYVLjL`,
     }).then(
         (data) => {
+            // finds lat and long for userInput
             const searchCoordinates = data.resourceSets[0].resources[0].geocodePoints[0].coordinates;
             $.ajax({
                 //url: `https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/search/geo/point?lat=30.267589569091797&lng=-97.74298858642578&radius=25&key=68855133f4391d6efa6f4d8fcf1b7674`,
-                url: 'https://cors-anywhere.herokuapp.com/https://www.brewerydb.com/browse/map/get-breweries?lat=30.267153&lng=-97.74306079999997&radius=25&search=austin%2C+tx'
+                url: `https://cors-anywhere.herokuapp.com/https://www.brewerydb.com/browse/map/get-breweries?lat=${searchCoordinates[0]}&lng=${searchCoordinates[1]}&radius=15`
             }).then(
                 (data) => {
-                    console.log(data);
+                    let dataInfo = JSON.parse(data);
+                    for(let i = 0; i < dataInfo.data.length; i++){
+                        console.log(dataInfo.data[i]);
+                        // console.log(dataInfo.data[i].brewery.name);
+                    }
+                    
                 },
                 () => {
                     console.log('bad request');
@@ -35,6 +41,7 @@ $('#submit-city').click(function(e){
         }
     );
 })
+
 
 
 
